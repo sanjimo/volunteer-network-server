b52ab61eb811f86efd53dfcab91f038080200598
Sanjida Islam <sanjimo123@gmail.com>
2021-10-27 18:58:31 +0600

    delete events added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,26 +10,45 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdilp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run(){
-   try{
+    try{
       await client.connect();
       //console.log("database connected!");
       const database = client.db('volunteer_network');
       const eventsCollection = database.collection('events');
 
       //get events api
-      app.get('/events', async(req,res)=>{
+        app.get('/events', async(req,res)=>{
           const cursor = eventsCollection.find({});
           const events = await cursor.toArray();
 
           res.send({events});
-      });
-   }
-   finally{
+        });
+
+
+        //get single event
+        app.get('/events/:id', async (req,res)=>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const event = await eventsCollection.findOne(query);
+           res.json(event);
+        });
+
+        //delete events
+        app.delete('/events/:id', async (req,res)=>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const result = await eventsCollection.deleteOne(query);
+           res.json(result);
+        });
+
+    }
+    finally{
        //await client.close();
-   }
+    }
 }
 
 run().catch(console.dir);
@@ -39,4 +59,4 @@ app.get('/', (req,res)=>{
 
 app.listen(port, ()=>{
     console.log('Running server port : ', port);
-})
+});
